d31d7f98ba014fc69c8d9238cce9e0091d71163f
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-24 15:13:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
        elsif array.count == 2 
         return [array].join(" and ") 
       else array.count != 1 || 2 
-        array.insert(-2, " and ")
+        array.insert(-2, "and ")
         return [array].join(", ")
       end
     end
